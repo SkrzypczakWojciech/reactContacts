8b3bc6305f6a3cd53ee245463c26259e5aeafe54
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-07-11 16:50:25 +0200

    add structure

---
+++ components/contact.js
@@ -0,0 +1,21 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('img', {
+          className: 'contactImage',
+          src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxC8CPYLc42Vx6QlVyaZ7TMzh-GhJxQmX3wc3sjHYDSrtvjUOK'
+        }),
+        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
