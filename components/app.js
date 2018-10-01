8599494a866ca2764865a0979aa7d0dff673686d
Wojciech Skrzypczak <skrzypczakwojciech@wp.pl>
2018-10-01 12:23:56 +0200

    small changes

--- components/app.js
+++ components/app.js
@@ -29,11 +29,9 @@ var App = React.createClass({
   render: function() {
     return (
       <div className= 'app'>
-        <ContactForm> 
-          contact = {contactForm}
+        <ContactForm contact = {contactForm}> 
         </ContactForm>
-        <Contacts> 
-          {items: contacts}
+        <Contacts items = {contacts}> 
         </Contacts>
       </div>
       )
